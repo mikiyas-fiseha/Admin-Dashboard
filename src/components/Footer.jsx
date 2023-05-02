d28a9b4992f79eb6a8ae55a379b68d472920e8e6
John Doe <mikiyasfiseha097@gmail.com>
2023-05-01 20:54:19 -0700

    copyright

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
+const today = new Date();
 const Footer = () => (
+  
   <div className="mt-24">
     <p className="dark:text-gray-200 text-gray-700 text-center m-20">
-      © 2022 All rights reserved by Shoppy.com
+      © {today.getFullYear()} All rights reserved by Mikiyas fiseha
     </p>
   </div>
 );
